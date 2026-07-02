cae4d7ad547adfc6506e494215bff19a9f489fb5
Samiksha Shreya <samikshashreya@Samikshas-MacBook-Pro-2.local>
2026-07-02 11:57:59 +0530

    chore: bump version 3.11.0 -> 4.0.0 (major, breaking paymentId) (LIN-1649)

--- Sources/Linkrunner/Linkrunner.swift
+++ Sources/Linkrunner/Linkrunner.swift
@@ -981,7 +981,7 @@ public class LinkrunnerSDK: @unchecked Sendable {
     }
     
     private func getPackageVersion() -> String {
-        return "3.12.0" // Swift package version
+        return "4.0.0" // Swift package version
     }
     
     private func getAppVersion() -> String {
